93135a729360457355721903eb536f13d3c84647
alif338 <alifm26072000@gmail.com>
2020-09-22 17:12:07 +0700

    first commit

---
+++ src/test/java/com/dicoding/FinalProject/FinalProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dicoding.FinalProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
